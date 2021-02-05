dd9d324fb6c5c21b8ef0d1fd5910f9a2f300ae5b
shibilm22 <shibil.mandawe@patelhs.org>
2021-02-05 14:58:56 +0000

    added comments

--- Main.java
+++ Main.java
@@ -7,19 +7,25 @@ class Main {
     //starting with three jobs 
     System.out.println("jobs available = " + totalJobs);
     //depending if you got the job or not it will either stay as 3 or subtract a 1 if you got the job
-    System.out.println("what is your name?");
+    System.out.println("Hello nice to meet you! What is your name?");
+    //greeting for the user
     String name = scan.next();
     System.out.println("what year is it?");
+    //this will be asked to calculate the age
     int currentyear = scan.nextInt();
     System.out.println("what is your birth year?");
+    //this will be asked to calculate the age
     int birthYear = scan.nextInt();
     int age = getAge(birthYear, currentyear);
     System.out.println("how many years of experience do you have?");
+    //this will allow the user to know if he or she is qualified 
     int yearsOfExperience = scan.nextInt();
     if (becameRapManager(age, yearsOfExperience)) {
       System.out.println("Yay! You are qualified to be a music manager!");
+      //this will only show up IF age, and experience passed
       totalJobs = jobsAvailable(totalJobs);
       System.out.println("jobs available = " + totalJobs);
+      //if qualified, the user will get a job and subtract the first amount of jobs by 1
 
     } else {
       System.out.println("Maybe next year you will be able to get the spot!");
@@ -30,13 +36,8 @@ class Main {
   // method calculates age
   static int getAge(int birthYear, int currentYear) {
     int age = currentYear - birthYear;
+    //this formula will calculate the age of the user.
     return age;
-  }
-
-  // method returns greeting
-  static String greeting(String name) {
-    String hello = "hello, nice to meet you! " + name;
-    return hello;
   }
 
   static boolean becameRapManager(int age, int experience) {
